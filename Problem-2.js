6f44c1a8da0228f3774c116e018096b54c746afa
Atanu8250 <atanu20uparsole@gmail.com>
2023-07-03 15:07:51 +0530

    Problem-2 done

--- Problem-2.js
+++ Problem-2.js
@@ -10,4 +10,29 @@ With a single integer as the input, generate the following until a = x [series o
         .
         .
         5) input a = x, then output : 1, 3, 5, 7, .......
-*/ 
+*/
+
+/**
+ * Generate `n` odd numbers.
+ * @param {number} n - Number of odd numbers to generate.
+ * @returns {number[]} - Array containing `n` odd numbers.
+ */
+function getOddNums(n = 1) {
+    // Create an array to store the odd numbers
+    let oddNumbers = new Array(n);
+  
+    // Generate the odd numbers using a loop
+    for (let i = 0; i < n; i++) {
+      oddNumbers[i] = 2 * i + 1;
+    }
+  
+    // Log the generated odd numbers to the console
+    console.log(`Output: ${oddNumbers.join(", ")}`);
+  
+    // Return the array of odd numbers
+    return oddNumbers;
+  }
+  
+  // Example usage
+  getOddNums(2);
+  
